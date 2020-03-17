6b2d343f1e88ebbc7eb1509e3def901281a519f6
Kuei <vx3012564897@gmail.com>
2020-03-18 00:53:02 +0800

    already finished project

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let diceArray = ["Dice1","Dice2","Dice3","Dice4","Dice5","Dice6"]
+    var diceIndex1 : Int = 0
+    var diceIndex2 : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImage()
     }
 
+    @IBOutlet weak var imgDice1: UIImageView!
+    
+    @IBOutlet weak var imgDice2: UIImageView!
+    @IBAction func rollDice(_ sender: Any) {
+        diceIndex1 = Int(arc4random_uniform(UInt32(6)))
+        //print("Dice1:\(diceIndex1)")
+        imgDice1.image = UIImage(named: diceArray[diceIndex1])
+        diceIndex2 = Int(arc4random_uniform(UInt32(6)))
+        //print("Dice2:\(diceIndex2)")
+        imgDice2.image = UIImage(named: diceArray[diceIndex2])
+    }
+    
+    func updateDiceImage(){
+        diceIndex1 = Int(arc4random_uniform(UInt32(diceArray.count)))
+        diceIndex2 = Int(arc4random_uniform(UInt32(diceArray.count)))
+        
+        imgDice1.image = UIImage(named: diceArray[diceIndex1])
+        imgDice2.image = UIImage(named: diceArray[diceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+//        if motion == UIEvent.EventSubtype.motionShake{
+//            updateDiceImage()
+//        }
+        
+        //簡寫法
+        if motion == .motionShake{
+            updateDiceImage()
+        }
+    }
 
 }
 
